bf63a2223c35f05c34cc30badc2b009861fd566d
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-11-25 01:27:25 +0100

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+    },
+
+    screens: {
+      'smaller': '450px',
+
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+      'midlaptop': '740px',
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    fontWeight: {
+      thin: '100',
+      extralight: '200',
+      light: '300',
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+      extrabold: '800',
+      black: '900',
+    }
+  },
+  plugins: [require("daisyui"), require('flowbite/plugin')],
+
+}

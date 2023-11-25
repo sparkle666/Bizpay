5c3a4486f1d41a9a44db51d749c0af239120f780
sparkle666 <sikky606@gmail.com>
2023-11-25 22:00:52 +0100

    feat: added the hero section plus assets

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,24 @@
+import { FC } from "react"
+import IPad from "../assets/iPadPro.png"
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
+const Hero: FC = () => {
+    return (
+        <div className="flex flex-col lg:flex-row px-10 py-10 justify-evenly items-center">
+            <div className="space-y-4">
+                <p className="text-[50px] font-semibold leading-none">Efficient payment management for small businesses</p>
+                <p>Our application allows small business owners to easily create <br />
+                    profiles, track payment transactions, and receive real-time <br />
+                    alerts for unpaid invoices</p>
+                <div className="space-x-3">
+                    <a href="" className="btn bg-secondary outline-0 text-white">Get Started</a>
+                    <a href="" className="btn-outline border-0 font-semibold">Learn More</a>
+                </div>
+            </div>
+            <div>
+                <img src={IPad} alt="An ipad image" />
+            </div>
+        </div>
+    )
 }
 
 export default Hero

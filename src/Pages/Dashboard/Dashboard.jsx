984a287a18dfc4b9a1e1aa780c6893f70dc9b715
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-11-25 02:02:26 +0100

    Setup

---
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+
+const Dashboard = () => {
+    return (
+        <section className='bg-gray-100 text-black'>
+            <div>
+                <h1 className='text-2xl'>
+                    Dashboard
+                </h1>
+            </div>
+        </section>
+    )
+}
+
+export default Dashboard

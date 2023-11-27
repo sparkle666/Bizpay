13c2771a40c295b5a31cb03d897db1f9573c44d6
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-11-27 16:13:59 +0100

    Update Invoice.tsx

--- src/pages/Invoice/Invoice.tsx
+++ src/pages/Invoice/Invoice.tsx
@@ -3,7 +3,7 @@ import { FiDownload } from "react-icons/fi";
 
 const Invoice = () => {
     return (
-        <section className='bg-gray-100 p-8 text-black'>
+        <section className='bg-gray-100 p-8 space-y-8 text-black'>
             <div className="md:flex justify-between">
                 <h1 className='text-4xl font-semibold'>
                     Recent Invoice
@@ -16,6 +16,10 @@ const Invoice = () => {
                         <FiDownload className="inline w-4 h-4" /> Export
                     </button>
                 </span>
+            </div>
+
+            <div className="text-center">
+                <i className="">Nothing to show here..</i>
             </div>
         </section>
     )
